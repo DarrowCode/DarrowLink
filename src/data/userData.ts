6f9a54f2bd0ccbd0cfe5d40e99329b2204ed4f5d
DarrowCode <darrowhdyt@gmail.com>
2024-09-29 14:06:21 +0200

    Add SocialLinks component and import social media links from userData

---
+++ src/data/userData.ts
@@ -0,0 +1,47 @@
+const userData = [
+  {
+    username: "admin",
+    userOccupation: "Admin",
+    userImage: "",
+  },
+];
+
+const socialMediaLinks = [
+    {
+        name: "GitHub",
+        link: ""
+    },
+    {
+        name: "LinkedIn",
+        link: ""
+    },
+    {
+        name: "Twitter",
+        link: ""
+    },
+    {
+        name: "Instagram",
+        link: ""
+    }
+];
+
+const userLinks = [
+    {
+        title: "Portfolio",
+        link:"",
+        icon:"ant-design:code-filled"
+    },
+    {
+        title: "Sucríbete a mi canal",
+        link:"",
+        icon:"mingcute:youtube-fill"
+
+    },
+    {
+        title: "Join Discord",
+        link:"",
+        icon:"ic:outline-discord"
+    },
+]
+
+export { userData, socialMediaLinks, userLinks };
